8154114ebf3c2d617d9d61eeea7df1bd02b68af3
Narin Luangrath <narin.luangrath@gmail.com>
2022-02-27 18:55:18 -0800

    temp: executeScript works

--- src/background.ts
+++ src/background.ts
@@ -1,6 +1,9 @@
-let color = '#3aa757';
-
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
-});
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  const tabId = sender.tab?.id;
+  if (tabId) {
+    chrome.scripting.executeScript({
+      target: { tabId },
+      func: () => console.log("hi"),
+    })
+  }
+})
--- src/content-script.ts
+++ src/content-script.ts
@@ -1,2 +1,1 @@
-console.log("I should run on every page!");
-console.log("Here is the chrome global", chrome);
+chrome.runtime.sendMessage('Page Loaded')
